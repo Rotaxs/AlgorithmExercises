2dabdb605cfcd2a7110d5d23ec38306038384ca3
Rotaxs <2076242097@qq.com>
2026-08-27 21:09:57 +0800

    Ex: nowcoder tarcker daily 20260827

---
+++ Exercises/Nowcoder/tracker_daily_20260827.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+void solve()
+{
+    int a; cin >> a;
+    if (a == 1) {
+        cout << 2 << ' ' << (a ^ 2) << endl;
+        return;
+    }
+    cout << 1 << ' ' << (a ^ 1) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int _ = 1; 
+    cin >> _;
+    while (_--) solve();
+    
+    return 0;
+}
